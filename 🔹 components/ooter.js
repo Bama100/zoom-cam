ff942f69b0222a92af5ad2b33246846d4cb0f98f
Bama100 <anamohamed411@gmail.com>
2025-07-08 23:55:15 +0200

    Create ooter.js

---
+++ 🔹 components/ooter.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-900 text-white py-4 text-center mt-10">
+      <p>© 2025 Zoom Cam - جميع الحقوق محفوظة 📱 01143249971</p>
+    </footer>
+  );
+}
